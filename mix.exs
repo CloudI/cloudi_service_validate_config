4b3be56b5117249d619f89b57f7a240cde5ab7a5
Michael Truog <mjtruog@protonmail.com>
2023-07-19 20:03:26 -0700

    Add LICENSE and mix.exs.

---
+++ mix.exs
@@ -0,0 +1,39 @@
+#-*-Mode:elixir;coding:utf-8;tab-width:2;c-basic-offset:2;indent-tabs-mode:()-*-
+# ex: set ft=elixir fenc=utf-8 sts=2 ts=2 sw=2 et nomod:
+
+defmodule CloudIServiceValidateConfig do
+  use Mix.Project
+
+  def project do
+    [app: :cloudi_service_validate_config,
+     version: "2.0.6",
+     language: :erlang,
+     erlc_options: [
+       :deterministic,
+       :debug_info,
+       :warn_export_vars,
+       :warn_unused_import,
+       :warn_missing_spec,
+       :warnings_as_errors],
+     description: description(),
+     package: package(),
+     deps: deps()]
+  end
+
+  defp deps do
+    [{:cloudi_core, ">= 2.0.6"}]
+  end
+
+  defp description do
+    "Erlang/Elixir Cloud Framework Validate Config Service"
+  end
+
+  defp package do
+    [files: ~w(src doc rebar.config README.markdown LICENSE),
+     maintainers: ["Michael Truog"],
+     licenses: ["MIT"],
+     links: %{"Website" => "https://cloudi.org",
+              "GitHub" => "https://github.com/CloudI/" <>
+                          "cloudi_service_validate_config"}]
+   end
+end
